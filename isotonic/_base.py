2137ef2eed4667a05267c6231e885bc29ce6cacd
Chris Stucchio <hi@chrisstucchio.com>
2020-03-05 17:40:41 -0800

    Abstract base class to handle core of isotonic algo.

---
+++ isotonic/_base.py
@@ -0,0 +1,49 @@
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
+from scipy.optimize import minimize
+import numpy as np
+from ._parameterization import gamma_of_alpha, grad_gamma_of_alpha
+from .curves import PiecewiseLinearIsotonicCurve
+import abc
+
+
+__all__ = ['AbstractIsotonicRegression']
+
+
+class AbstractIsotonicRegression(ClassifierMixin, TransformerMixin, BaseEstimator, metaclass=abc.ABCMeta):
+    def __init__(self, npoints, increasing=True, cut_algo='quantile', curve_algo=PiecewiseLinearIsotonicCurve):
+        assert (cut_algo in ['quantile', 'uniform'])
+        self.npoints = npoints
+        self.cut_algo = cut_algo
+        self.increasing = increasing
+        self.curve_algo = curve_algo
+        self.fitted_ = False
+
+    @abc.abstractmethod
+    def _err_func(self, x_cuts, X,y):
+        pass
+
+    @abc.abstractmethod
+    def _grad_err_func(self, x_cuts, X,y):
+        pass
+
+    def fit(self, X, y):
+        if self.cut_algo == 'quantile':
+            x_cuts = np.quantile(X, np.arange(0,1,1/self.npoints))
+        else:
+            x_cuts = X.min() + (X.max() - X.min())*np.arange(0,1,1/self.npoints)
+        alpha = np.zeros(shape=(int(self.npoints+1),))
+
+        err = self._err_func(x_cuts, X, y)
+        grad_err = self._grad_err_func(x_cuts, X, y)
+
+        min_result = minimize(err, x0=alpha, method='CG', jac=grad_err)
+        self.curve_ = self.curve_algo(x_cuts, gamma_of_alpha(min_result.x), increasing=self.increasing)
+        self.fitted_ = True
+        return self
+
+    def transform(self, X):
+        assert self.fitted_
+        return self.curve_.f(X)
+
+    def predict_proba(self, X):
+        return self.transform(X)
--- isotonic/binomial_isotonic_regression.py
+++ isotonic/binomial_isotonic_regression.py
@@ -10,28 +10,16 @@ __all__ = ['BinomialIsotonicRegression']
 
 
 class BinomialIsotonicRegression(AbstractIsotonicRegression):
-    def __init__(self, npoints, increasing=True, cut_algo='quantile', curve_algo=PiecewiseLinearIsotonicCurve):
-        assert (cut_algo in ['quantile', 'uniform'])
-        self.npoints = npoints
-        self.cut_algo = cut_algo
-        self.increasing = increasing
-        self.curve_algo = curve_algo
-        self.fitted_ = False
-
-    def fit(self, X, y):
-        if self.cut_algo == 'quantile':
-            x_cuts = np.quantile(X, np.arange(0,1,1/self.npoints))
-        else:
-            x_cuts = X.min() + (X.max() - X.min())*np.arange(0,1,1/self.npoints)
-        alpha = np.zeros(shape=(int(self.npoints+1),))
-
+    def _err_func(self, x_cuts, X, y):
         def err(alpha):
             gamma = gamma_of_alpha(alpha)
             curve = self.curve_algo(x=x_cuts, y=gamma, increasing=self.increasing)
             p = curve.f(X)
             result = -1*(np.log(p[y == 1]).sum() + np.log(1-p[y==0]).sum())
             return result / len(X)
+        return err
 
+    def _grad_err_func(self, x_cuts, X, y):
         def grad_err(alpha):
             gamma = gamma_of_alpha(alpha)
 
@@ -42,13 +30,4 @@ class BinomialIsotonicRegression(AbstractIsotonicRegression):
             dE_dgamma[y == 0] = -1.0/(1-p[y == 0])
             dE_dgamma = curve.grad_y(X) @ dE_dgamma
             return -1*grad_gamma_of_alpha(alpha) @ dE_dgamma / len(X)
-
-        min_result = minimize(err, x0=alpha, method='CG', jac=grad_err)
-        print(min_result)
-        self.curve_ = self.curve_algo(x_cuts, gamma_of_alpha(min_result.x), increasing=self.increasing)
-        self.fitted_ = True
-        return self
-
-    def predict_proba(self, X):
-        assert self.fitted_
-        return self.curve_.f(X)
+        return grad_err
